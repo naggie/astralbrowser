eac96063e3f2218f9090f72c5b2163c68ab783f3
Callan Bryant <callan.bryant@gmail.com>
2022-03-10 21:57:53 +0000

    working async/await based loading

--- src/LsDir.svelte
+++ src/LsDir.svelte
@@ -4,17 +4,16 @@
 
     export let path: string = "/";
 
-    // Unresolved promise until first request
-    let req: Promise<Listing> = new Promise(() => {});
+    let listingReq: Promise<Listing>;
 
-    async function load_path(path: string) {
+    async function load_path(path: string) : Promise<Listing> {
         // must end in a slash to avoid loading massive non-directories. Set path to reflect in UI
         path = joinPath('/', path, '/');
 
         // can get annoying if we don't do this
         window.scroll(0,0);
 
-        req = fetch(
+        const response = await fetch(
             joinPath(base, path),
             {
                 headers: {
@@ -22,21 +21,19 @@
                 },
             }
         )
-        .then(response => {
-            if (response.status == 404) {
-                throw new Error("Directory does not exist");
-            } else if (response.status != 200) {
-                throw new Error("Error loading directory");
-            }
+        if (response.status == 404) {
+            throw new Error("Directory does not exist");
+        } else if (response.status != 200) {
+            throw new Error("Error loading directory");
+        }
 
-            return response.json();
-        });
+        return response.json();
     }
 
-    $: load_path(path);
+    $: listingReq = load_path(path);
 </script>
 
-{#await req}
+{#await listingReq}
 <div class="accesswait"><div class="progress-line"></div></div>
 {:then listing}
 <table>
