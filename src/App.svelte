8d61f89b55be8170f75619c06100d22982a58c55
Callan Bryant <callan.bryant@gmail.com>
2022-03-10 19:36:56 +0000

    working separation of search and path

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,13 @@
 <script lang="ts">
-    import { tick } from 'svelte';
     import { hash } from './stores';
-    import { humanFileSize, humanRelativeTime, joinPath, parentDir } from './util';
+    import LsDir from './LsDir.svelte';
+    import { joinPath } from './util';
     export let base: string;
     // show for / only
     export let readme: HTMLElement;
 
     let path: string = "/";
     let search: string = "";
-
-    let req: Promise<Listing> | undefined;
-
-    async function load_path(path: string) {
-        // can get annoying if we don't do this
-        window.scroll(0,0);
-
-        if (path != "/") readme.style.display = "none";
-
-        req = fetch(
-            joinPath(base, path),
-            {
-                headers: {
-                    'Accept': 'application/json',
-                },
-            }
-        )
-        .then(response => {
-            if (response.status == 404) {
-                throw new Error("Directory does not exist");
-            } else if (response.status != 200) {
-                throw new Error("Error loading directory");
-            }
-
-            // show readme if appropriate (after load so it does not jump)
-            if (path == "/") readme.style.display = "block";
-
-            return response.json();
-        });
-    }
-
-    function load_search(query: string) {
-        console.log(query);
-    }
 
     function handlePathSubmit(e: Event) {
         window.location.hash = joinPath("/", e.target.elements["path"].value, "/");
@@ -53,14 +19,15 @@
 
     $: if ($hash.startsWith("?")) {
         search = $hash.slice(1);
-        load_search(search);
         path = "";
     } else {
         // must end in a slash to avoid loading massive non-directories. Set path to reflect in UI
         path = joinPath('/', $hash, '/');
-        load_path(path);
         search = "";
     }
+
+    // show readme if appropriate (after load so it does not jump)
+    $: readme.style.display = path == '/' ? "block" : "none";
 </script>
 
 <div id="astralbrowser-toolbar">
@@ -74,46 +41,13 @@
     </form>
 </div>
 
-{#await req}
-<div class="accesswait"><div class="progress-line"></div></div>
-{:then listing}
-<table>
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Size</th>
-        <th>Modified</th>
-      </tr>
-    </thead>
-    <tbody>
-    {#if path != "/"}
-      <tr>
-        <td><a href={'#' + parentDir(path)}>../</a></td>
-        <td>-</td>
-        <td>-</td>
-      </tr>
-    {/if}
-    {#each listing as item}
-        {#if item.type == "directory"}
-          <tr>
-            <td><a href={'#' + joinPath(path, item.name, "/")}>{joinPath(item.name, "/")}</a></td>
-            <td>-</td>
-            <td>{humanRelativeTime(item.mtime)}</td>
-          </tr>
-        {:else if item.type == "file"}
-          <tr>
-            <td><a href={joinPath(base, path, item.name)} download>{item.name}</a></td>
-            <td>{humanFileSize(item.size)}</td>
-            <td>{humanRelativeTime(item.mtime)}</td>
-          </tr>
-        {/if}
-    {/each}
-    </tbody>
-</table>
-
-{:catch error}
-<div class="nis nis-error">{error.message}</div>
-{/await}
+{#if path}
+<LsDir base={base} path={path} />
+{:else if search}
+Search query: {search}
+{:else}
+Nothing to do.
+{/if}
 
 <style>
     #astralbrowser-toolbar {
