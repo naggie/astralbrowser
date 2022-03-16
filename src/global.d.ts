87d63c12b04b6d949913cd84cd4745e634a6ae1d
Callan Bryant <callan.bryant@gmail.com>
2022-03-16 20:02:32 +0000

    make type defs get picked up

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,24 +1,32 @@
 /// <reference types="svelte" />
-export interface Directory {
-    type: "directory";
-    name: string;
-    mtime: string;
+declare global {
+    interface Directory {
+        type: "directory";
+        name: string;
+        mtime: string;
+    }
+
+    interface File {
+        name: string;
+        type: "file";
+        mtime: string;
+        size: number;
+    }
+
+    type Listing = Array<File|Directory>;
+
+    interface ProgressReport {
+        searching: boolean;
+        numSearched: number;
+        numResults: number;
+        // approximated via bytes of full content length
+        percentSearched: number;
+        percentIndexDownloaded: number;
+        elapsedMs: number;
+        // current query, so it's possible to tell if the search engine is behind
+        // and make the UI reflect that
+        query: string;
+    }
 }
 
-export interface File {
-    name: string;
-    type: "file";
-    mtime: string;
-    size: number;
-}
-
-export type Listing = Array<File|Directory>;
-
-export interface ProgressReport {
-    searching: boolean;
-    numSearched: number;
-    numResults: number;
-    // approximated via bytes of full content length
-    percentSearched: number;
-    elapsedMs: number;
-}
+export {}
