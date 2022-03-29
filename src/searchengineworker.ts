e1826fb4a6943c820ea1d22deda5dc24ac328a63
Callan Bryant <callan.bryant@gmail.com>
2022-03-29 02:59:45 +0100

    workign worker commmunicaiton

--- src/searchengineworker.ts
+++ src/searchengineworker.ts
@@ -1,13 +1,12 @@
 import SearchEngine from './searchengine';
 // Simple Web Worker wrapper for search engine to allow for a completely unblocked UI.
-
-// TODO consider https://www.npmjs.com/package/rollup-plugin-web-worker-loader
-// to avoid additional request in the critical path
 // https://www.jameslmilner.com/post/workers-with-webpack-and-typescript/
+
+let searchEngine: SearchEngine;
 
 self.addEventListener("message", (e) => {
     const cmd: WorkerCmd = e.data;
-    let searchEngine: SearchEngine;
+    console.log(cmd);   // TODO remove
 
     switch(cmd.type) {
         case "init":
