0a0e6ff22c5b7e01311d4350bfc1e46dbc88d32b
Callan Bryant <callan.bryant@gmail.com>
2022-04-13 02:13:55 +0100

    add indexer, untested

--- install.sh
+++ install.sh
@@ -4,8 +4,10 @@ cd $(dirname $0)
 
 # TODO ensure NTP time sync across hosts (not here!)
 
-
 sudo cp mounts/*.mount /etc/systemd/system/
+sudo cp ***REMOVED***astralbrowser-indexer.{service,timer} /etc/systemd/system/
+sudo cp poc-indexer /opt/***REMOVED***/bin
+
 
 # timer triggered services (--now first run only)
 # regardless of status.
@@ -13,3 +15,4 @@ sudo systemctl daemon-reload
 
 # mount (they will be automounted on boot)
 sudo systemctl start $(ls mounts/)
+sudo systemctl enable --now ***REMOVED***astralbrowser-indexer.timer
