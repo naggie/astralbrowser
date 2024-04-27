0c188a8cdbfe2747bf6fef4cc211b599ddf77b8e
Callan Bryant <callan.bryant@gmail.com>
2024-04-27 19:00:49 +0100

    hack: re-order so timesyncd sodesn't bail the script early

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ sudo apt-get -y install nfs-common
 sudo cp ***REMOVED***astralbrowser-indexer.{service,timer} /etc/systemd/system/
 sudo cp poc-indexer /opt/***REMOVED***/bin
 
+sudo ./mkmount oxygen.***REMOVED***.io:/srv/media /mnt/depot
+sudo ./mkmount dilithium.***REMOVED***.io:/data/shared /mnt/shares/naggie
+sudo ./mkmount tempestkeep.frillnet.***REMOVED***.io:/srv/media /mnt/shares/frillip
+
 # enable ntp, nfs works best with sychronised clocks
 sudo cp timesyncd.conf /etc/systemd/
 sudo systemctl stop systemd-timesyncd.service
@@ -18,6 +22,3 @@ sudo systemctl enable --now systemd-timesyncd.service
 sudo systemctl daemon-reload
 sudo systemctl enable --now ***REMOVED***astralbrowser-indexer.timer
 
-sudo ./mkmount oxygen.***REMOVED***.io:/srv/media /mnt/depot
-sudo ./mkmount dilithium.***REMOVED***.io:/data/shared /mnt/shares/naggie
-sudo ./mkmount tempestkeep.frillnet.***REMOVED***.io:/srv/media /mnt/shares/frillip
