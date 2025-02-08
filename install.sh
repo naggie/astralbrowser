b486ea8e652a699307b56ed7d330f34a17447f30
Callan Bryant <callan.bryant@gmail.com>
2025-02-08 14:53:14 +0000

    mounts

--- install.sh
+++ install.sh
@@ -8,12 +8,15 @@ sudo apt-get -y install nfs-common
 sudo cp ***REMOVED***astralbrowser-indexer.{service,timer} /etc/systemd/system/
 sudo cp astralbrowser-indexer /opt/***REMOVED***/bin
 
-sudo ./mkmount oxygen.***REMOVED***.io:/data/nfs /mnt/astralbrowser/nfs
 sudo ./mkmount oxygen.***REMOVED***.io:/data/downloads /mnt/astralbrowser/downloads
 sudo ./mkmount oxygen.***REMOVED***.io:/data/software /mnt/astralbrowser/software
 sudo ./mkmount oxygen.***REMOVED***.io:/data/movies /mnt/astralbrowser/movies
 sudo ./mkmount oxygen.***REMOVED***.io:/data/tv /mnt/astralbrowser/tv
 sudo ./mkmount oxygen.***REMOVED***.io:/data/music /mnt/astralbrowser/music
+
+sudo ./mkmount dilithium.***REMOVED***.io:/data/shared /mnt/astralbrowser/nfs/naggie
+sudo ./mkmount tempestkeep.frillnet.***REMOVED***.io:/srv/media /mnt/astralbrowser/nfs/frillip
+sudo ./mkmount 172.18.64.22:/mnt/user/media /mnt/astralbrowser/nfs/votick
 
 sudo mkdir -p /mnt/astralbrowser
 sudo chown www-data:www-data /mnt/astralbrowser
