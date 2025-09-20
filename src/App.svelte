7b3f6ffc6b17389db4f3843fe3dea2972f457dd5
Callan Bryant <callan.bryant@gmail.com>
2025-09-20 12:12:15 +0100

    revert: clear query on path nav

--- src/App.svelte
+++ src/App.svelte
@@ -67,9 +67,6 @@
         // must end in a slash to avoid loading massive non-directories. Set path to reflect in UI
         path = joinPath('/', $hash, '/');
         query = "";
-        if ($hash.startsWith("/")) {
-            inputQuery = "";
-        }
     }
 
     $: searchEngineWorker.postMessage({type:"newSearch", query: query});
