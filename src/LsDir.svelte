52d0334754d1d3026fceafaa1676331651be62c3
Callan Bryant <callan.bryant@gmail.com>
2025-09-21 18:01:36 +0100

    README.md support

--- src/LsDir.svelte
+++ src/LsDir.svelte
@@ -6,8 +6,12 @@
     export let path: string = "/";
 
     let listingReq: Promise<Listing>;
+    let readme: string = "";
 
     async function load_path(path: string) : Promise<Listing> {
+        // invalidate readme
+        readme = "";
+
         // must end in a slash to avoid loading massive non-directories. Set path to reflect in UI
         path = joinPath('/', path, '/');
 
@@ -28,7 +32,19 @@
             throw new Error("Error loading directory");
         }
 
-        return response.json();
+        const listing: Listing = Array.from(await response.json());
+
+        // look for README.md and remove it from listing, download and display it later
+        for (let i = 0; i < listing.length; i++) {
+            if (listing[i].name == "README.md" && listing[i].type == "file") {
+                listing.splice(i, 1);
+                const readmeResp = await fetch( joinPath(mountPoint, path, "README.md"));
+                readme = await readmeResp.text();
+                break; // important to break here, otherwise i may be out of bounds
+            }
+        }
+
+        return listing;
     }
 
     $: listingReq = load_path(path);
@@ -41,3 +57,9 @@
 {:catch error}
 <p class="warningbox">{error.message}</p>
 {/await}
+
+{#if readme}
+<strong>README.md</strong>
+<br/>
+<pre><code>{readme}</code></pre>
+{/if}
