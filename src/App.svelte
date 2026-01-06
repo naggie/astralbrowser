3da77f49442269dcfeef15cb6e6cedecb8e69d4b
callan <callan.bryant@gmail.com>
2026-01-06 22:09:55 +0000

    inline worker

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
     import { hash } from './stores';
     import LsDir from './LsDir.svelte';
     import { joinPath } from './util';
-    import SearchEngineWorker from './searchengineworker?worker';
+    import SearchEngineWorker from './searchengineworker?worker&inline';
     import SearchResultsView from './SearchResultsView.svelte';
     import { onMount } from 'svelte';
     // path is what user sees, mountpoint is where path exists
